db65576082a20d0e8c9380161a5cf6a234fb2a39
Hangur <hanna.gurzeda@gmail.com>
2020-05-09 21:26:09 +0200

    Work in progress

---
+++ test/cipher.spec.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+import Cipher from '../scripts/cipher';
+
+describe('cipher.js', () => {
+    it('1 plus 1 is 2', () => {
+        console.log(Cipher);
+    });
+});
